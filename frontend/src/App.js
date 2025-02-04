1a4afd52605e62206ad0e6e18d47523ac98af8f9
MoAwad <moawad@MacBook-Pro-von-MoAwad.local>
2025-02-04 07:38:22 +0100

    Initial commit with frontend files

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import LoginCustomer from "./Components/LoginCustomer";
+import LoginRestaurant from "./Components/LoginRestaurant";
+
+import Signup from "./Components/Signup";
+import SignupCustomer from "./Components/SignupCustomer";
+import SignupRestaurant from "./Components/SignupRestaurant";
+
+
+
+
+
+
+function App() {
+  return <>
+          <Navbar />
+          <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="customer/login" element={<LoginCustomer />} />
+          <Route path="/restaurant/login" element={<LoginRestaurant />} />
+
+          <Route path="signup" element={<Signup />} />
+          <Route path="customer" element={<SignupCustomer />} />
+          <Route path="restaurant" element={<SignupRestaurant />} />
+
+
+
+
+
+
+          </Routes>
+
+  </>
+}
+
+export default App;
